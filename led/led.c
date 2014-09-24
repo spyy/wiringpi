23726a638ff5357733635286f76c3b9309e3fe36
spyy <sami.pyy@gmail.com>
2014-09-24 09:02:13 +0300

    button

--- led/led.c
+++ led/led.c
@@ -2,6 +2,7 @@
 #include<wiringPi.h>
 #include <unistd.h>
 
+
 int main(void){
 int i;
 printf("wiring setup..\n");
@@ -9,14 +10,19 @@ i = wiringPiSetupGpio();
 printf("wiring initialized: %d\n", i);
 
 pinMode(17,OUTPUT);
+pinMode(2,INPUT);
 
-for(i=0; i<99; i++){
-printf("Write HIGH\n");
-digitalWrite(17,HIGH);
-sleep(2);
-printf("Write LOW\n");
-digitalWrite(17,LOW);
-sleep(2);
+while(1){
+//i = waitForInterrupt(2, -1);
+//printf("waitForInterrupt: %d\n", i);
+sleep(1);
+i = digitalRead(2);
+if(i == HIGH){
+digitalWrite(17, LOW);
+}else{
+digitalWrite(17, HIGH);
+}
+
 }
 
 
