3c23c53e8a5ef85bc76e62bd604619597a3bfe29
unknown <ely@TYOD-ATK14-C03.kk.lan>
2014-09-24 10:21:24 +0300

    cleaning

--- led/led.c
+++ led/led.c
@@ -1,30 +1,26 @@
 #include<stdio.h>
 #include<wiringPi.h>
-#include <unistd.h>
-
+#include<unistd.h>
 
 int main(void){
-int i;
-printf("wiring setup..\n");
-i = wiringPiSetupGpio();
-printf("wiring initialized: %d\n", i);
+    int i;
+    printf("wiring setup..\n");
+    i = wiringPiSetupGpio();
+    printf("wiring initialized: %d\n", i);
 
-pinMode(17,OUTPUT);
-pinMode(2,INPUT);
+    pinMode(17,OUTPUT);
+    pinMode(2,INPUT);
 
-while(1){
-//i = waitForInterrupt(2, -1);
-//printf("waitForInterrupt: %d\n", i);
-sleep(1);
-i = digitalRead(2);
-if(i == HIGH){
-digitalWrite(17, LOW);
-}else{
-digitalWrite(17, HIGH);
-}
+    while(1){        
+        sleep(1);
+        i = digitalRead(2);
+        printf("digitalRead: %d\n", i);
+        if(i == HIGH){
+            digitalWrite(17, LOW);
+        }else{
+            digitalWrite(17, HIGH);
+        }
+    }
 
-}
-
-
-return 0;
+    return 0;
 }
